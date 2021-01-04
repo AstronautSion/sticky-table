1fc296fba32af6c93c95275118324b7cb5a93047
Astronaut.sion <apple@Appleui-MacBookPro.local>
2021-01-05 02:18:39 +0900

    add confirmType

--- src/js/sticky-table.js
+++ src/js/sticky-table.js
@@ -1,25 +1,83 @@
-class stickyTable {
+class StickyTable{
     constructor(option) {
-        this.init(option);
-        this.event();
+        this.target = null;
+        this.datas = null;
+        this.responsive = null;
+        this.stickyCol = null;
+        this.listCount = null;
+        this.pageCount = null;
+
+        this.confirmType(option);    
+    }
+
+    confirmType(option){
+        
+        var confirmOption = (callback) =>{
+            if(typeof option == 'object' ){ 
+                callback();
+            }else{ 
+                throw 'option is not object';
+            }
+        };
+        var confirmOptionTarget = (callback) =>{
+            if(document.getElementById(option.target)){
+                this.target = document.getElementById(option.target);
+                callback();
+            }else{
+                throw 'target id not defined';
+            }
+        };
+        var confirmDatas = (callback) => {
+            if(typeof option.datas == 'object'){
+                 if(Object.keys(option.datas[0])){
+                     console.log(Object.keys(option.datas[0]))
+                    if( Object.keys(option.datas[0]).length == 2){
+                        //head, body type 확인 하기.
+                        callback();
+                    }else{
+                        throw 'plz create option.datas.head or option.datas.body';
+                    }
+                 }else{
+                     throw 'plz create option.datas.head or option.datas.body';
+                 }
+            }else{
+                throw 'option.datas wrong type';
+            }
+        };
+
+        try {
+            confirmOption(()=>{
+                confirmOptionTarget(()=>{
+                    confirmDatas(()=>{
+                        this.init(option);
+                        this.event();    
+                    });
+                });
+            })
+        
+        } catch (e) {
+            console.error(e);
+            return;
+        }
     }
     init(option){
-        this.targetId = option.targetId;
         this.datas = option.datas[0];
         this.responsive = option.responsive;
         this.stickyCol = option.stickyCol;
         this.listCount = option.listCount || 10;
-        this.pageCount = option.pageCount || 10;
+        this.pageCount = option.pageCount || 10;   
+    }
+    confirmTarget(){
+
     }
     event(){
-        let table = document.getElementById(this.targetId);
-        this.createTableLayout(table);
+        this.createTableLayout();
     }
     createPagination(){
 
     }
     
-    createTableLayout(table){
+    createTableLayout(){
         let rowNum = this.datas.head.length;
         let colNum = this.datas.body.length
 
@@ -43,8 +101,8 @@ class stickyTable {
                 let tdStickyBodyHtml = '';
                 for(let j=0; j<bodyKeys.length; j++){     
                     (!this.stickyCol || j < this.stickyCol) ? 
-                    (tdBodyHtml += `<td>${this.datas.body[i][bodyKeys[j]]}</td>`) : 
-                    ( tdStickyBodyHtml += `<td>${this.datas.body[i][bodyKeys[j]]}</td>` );
+                    (tdBodyHtml += `<td>${this.datas.body[i][j]}</td>`) : 
+                    ( tdStickyBodyHtml += `<td>${this.datas.body[i][j]}</td>` );
                 }
                 bodyHtml += `<tr>${tdBodyHtml}</tr>`;
                 if( this.stickyCol ){ stickyBodyHtml += `<tr>${tdStickyBodyHtml}</tr>`; }
@@ -59,15 +117,18 @@ class stickyTable {
         }
 
         if(!this.stickyCol){
-            table.innerHTML = `<thead><tr>${createContents().headHtml}</tr></thead><tbody>${createContents().bodyHtml}</tbody>`;
+            this.target.innerHTML = `<thead><tr>${createContents().headHtml}</tr></thead><tbody>${createContents().bodyHtml}</tbody>`;
             if(this.responsive){ this.wrapDiv(table, 'responsive-table'); }
 
         }else{
-            table.innerHTML = `<thead><tr>${createContents().headHtml}</tr></thead><tbody>${createContents().bodyHtml}</tbody>`;
+            this.target.innerHTML = `<thead><tr>${createContents().headHtml}</tr></thead><tbody>${createContents().bodyHtml}</tbody>`;
             let othersHtml = `<table class="table"><thead><tr>${createContents().stickyHeadHtml}</tr></thead><tbody>${createContents().stickyBodyHtml}</tbody></table>`;
-            let stickyTable = this.wrapDiv(this.wrapDiv(table, 'table-area table-area1'), 'sticky-table');
+            let stickyTable = this.wrapDiv(this.wrapDiv(this.target, 'table-area table-area1'), 'sticky-table');
             stickyTable.insertAdjacentHTML('beforeend', `<div class="table-area table-area2">${othersHtml}</div>`);   
-        } 
+        }
+        
+
+         
     }
 
     wrapDiv(table,className=''){
